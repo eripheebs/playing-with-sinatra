593250caf5befb77dea62304038f5362f0446cae
eripheebs <erikapheby@gmail.com>
2016-03-15 11:03:53 +0000

    Playing around with the web

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+get '/' do
+  "Hello world"
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/naming' do
+  erb(:naming)
+end
